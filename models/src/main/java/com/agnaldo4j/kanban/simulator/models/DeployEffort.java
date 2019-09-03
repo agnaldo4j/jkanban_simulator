37792ffa1765f89a85690e1a3d284ebbfe307efe
agnaldo4j <agnaldo4j@gmail.com>
2019-09-03 15:53:20 -0300

    Fixed serialization number of class

--- models/src/main/java/com/agnaldo4j/kanban/simulator/models/DeployEffort.java
+++ models/src/main/java/com/agnaldo4j/kanban/simulator/models/DeployEffort.java
@@ -2,7 +2,7 @@ package com.agnaldo4j.kanban.simulator.models;
 
 public class DeployEffort extends TaskAbilityEffort {
 
-    private static final long serialVersionUID = -7539352185357721960L;
+    private static final long serialVersionUID = -1333117483364273218L;
 
     public DeployEffort(EffortType effortType) {
         super(effortType, MemberAbility.Deployer);
